3ac28f83f4fde85faf4d61faab34e3884c4b655b
Rcoder-singh <rishabh22.singh@gmail.com>
2024-09-18 01:40:11 +0530

    Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -69,69 +69,6 @@ const Navbar = () => {
         </li>
       </ul>
     </div>
-    // <nav className="navbar  bg-dark bg-slate-950">
-    //   <div className="container-fluid">
-    //     <a className="navbar-brand" href="#">
-    //       Navbar
-    //     </a>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarSupportedContent"
-    //       aria-controls="navbarSupportedContent"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon" />
-    //     </button>
-    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    //       <form className="d-flex ms-auto">
-    //         <input
-    //           className="form-control me-2"
-    //           type="search"
-    //           placeholder="Search"
-    //           aria-label="Search"
-    //         />
-    //         <button className="btn btn-outline-success" type="submit">
-    //           Search
-    //         </button>
-    //       </form>
-    //       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-    //         <li className="nav-item">
-    //           <Link className="nav-link active" aria-current="page" to="/">
-    //             Home
-    //           </Link>
-    //         </li>
-    //         <li className="nav-item">
-    //           <Link className="nav-link" to="/about">
-    //             About
-    //           </Link>
-    //         </li>
-    //         <li className="nav-item">
-    //           <Link className="nav-link" to="/cart">
-    //             Cart
-    //           </Link>
-    //         </li>
-    //         <li className="nav-item">
-    //           <Link className="nav-link" to="/contact">
-    //             Contact
-    //           </Link>
-    //         </li>
-    //         <li className="nav-item">
-    //           <Link className="nav-link" to="/login">
-    //             Login
-    //           </Link>
-    //         </li>
-    //         <li className="nav-item">
-    //           <Link className="nav-link" to="/signup">
-    //             Sign-up
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   );
 };
 
